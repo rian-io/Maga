da65d8166287dd604cc747949caaf2ba4558f7bb
Rian I. de Oliveira <rian-io@users.noreply.github.com>
2024-01-07 14:54:36 -0300

    Change input system

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,38 +1,57 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Transform _target;
 
     [Header("Zoom")]
-    [SerializeField, Range(10, 45)] private float _currentZoom = 25.0f;
+    [SerializeField] private float _currentZoom = 25.0f;
     [SerializeField] private float _zoomSpeed = 0.0f;
-    [SerializeField] private float _minZoom = 0.0f;
+    [Space][SerializeField] private float _minZoom = 0.0f;
     [SerializeField] private float _maxZoom = 0.0f;
 
     [Header("Rotation")]
-    [SerializeField] private float _rotationSpeed = 0.0f;
-    [SerializeField] private float _minVertAngle = 0.0f;
+    [SerializeField] private float _rotationTime = 0.0f;
+    [Space][SerializeField] private float _rotationHSpeed = 0.0f;
+    [SerializeField] private float _rotationVSpeed = 0.0f;
+    [Space][SerializeField] private float _minVertAngle = 0.0f;
     [SerializeField] private float _maxVertAngle = 0.0f;
 
     private Vector3 _localRotation;
 
+    private InputSystem _inputActions;
+
+    private void Awake()
+    {
+        _inputActions = new InputSystem();
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Disable();
+    }
+
     private void LateUpdate()
     {
-        _currentZoom -= Input.GetAxis("Mouse ScrollWheel") * _zoomSpeed;
+        _currentZoom -= _inputActions.Camera.Zoom.ReadValue<float>() * _zoomSpeed;
         _currentZoom = Mathf.Clamp(_currentZoom, _minZoom, _maxZoom);
 
-        if (Input.GetMouseButton(1))
+        if (_inputActions.Camera.EnableControl.IsPressed())
         {
-            _localRotation.y += Input.GetAxis("Mouse X") * _rotationSpeed;
-            _localRotation.x -= Input.GetAxis("Mouse Y") * _rotationSpeed;
+            Vector2 deltaMouse = _inputActions.Camera.Control.ReadValue<Vector2>().normalized;
+
+            _localRotation.y += deltaMouse.x * _rotationHSpeed;
+            _localRotation.x -= deltaMouse.y * _rotationVSpeed;
 
             _localRotation.x = Mathf.Clamp(_localRotation.x, _minVertAngle, _maxVertAngle);
 
-            Quaternion qt = Quaternion.Euler(_localRotation.x, _localRotation.y, 0f);
-            transform.rotation = Quaternion.Lerp(transform.rotation, qt, Time.deltaTime * 10);
+            Quaternion rotation = Quaternion.Euler(_localRotation.x, _localRotation.y, 0f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, _rotationTime);
         }
 
         transform.position = _target.position - transform.forward * _currentZoom;
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,22 +7,32 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _speed = 3.5f;
 
     private NavMeshAgent _agent;
-    private Camera _cam;
+    private InputSystem _inputActions;
 
-    // Start is called before the first frame update
-    private void Start()
+    private void Awake()
     {
-        _cam = Camera.main;
         _agent = GetComponent<NavMeshAgent>();
         _agent.speed = _speed;
+
+        _inputActions = new InputSystem();
+    }
+
+    private void OnEnable()
+    {
+        _inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Disable();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (_inputActions.Player.Move.IsPressed())
         {
-            Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray.origin, ray.direction, out var hitInfo))
             {
                 _agent.destination = hitInfo.point;
